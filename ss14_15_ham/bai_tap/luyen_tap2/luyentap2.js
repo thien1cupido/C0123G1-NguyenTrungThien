70ab461b54abcf23af3d3f798d52cd56bfd86559
thien1cupido <thien7028@gmail.com>
2023-02-20 11:32:32 +0700

    up bai

--- ss14_15_ham/bai_tap/luyen_tap2/luyentap2.js
+++ ss14_15_ham/bai_tap/luyen_tap2/luyentap2.js
@@ -1,17 +1,70 @@
 //Bình phương một số
-function square(number) {
-    let result = number * number;
-    return result;
+function getSquare(number) {
+    return number * number;
 }
+
 //Diện tích hình tròn
-function circleArea(number) {
-    const Pi=3.14;
-   let acreage=square(number)*Pi;
-   return acreage;
+function getCircleArea(number) {
+    const Pi = 3.14;
+    return getSquare(number) * Pi;
 }
+
 //Chu vi hình tròn
-function diameterOfCircle(number) {
-    const Pi=3.14;
-    let perimeter = number*Pi;
-    return perimeter;
+function getDiameterOfCircle(number) {
+    const Pi = 3.14;
+    return number * Pi;
+}
+
+//Tính giai thừa
+function getFactorial(number) {
+    let factorial = 1
+    for (let i = 1; i <= number; i++) {
+        factorial = i * factorial;
+    }
+    return factorial;
+}
+
+//Kiểm tra ký tự số
+function checkIsNumber(number) {
+    let check;
+    check = !isNaN(number);
+    return check;
+}
+
+//Trả về số nhỏ nhất
+function checkNumberSmallest(arr) {
+    let min = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (min > arr[i]) {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+// Kiểm tra số nguyên dương
+function checkForPositiveIntegers(number) {
+    return number > 0;
+}
+
+// Đảo mảng
+function reverseArray(str) {
+    let string = str.split("");
+    let arrStr = string.reverse();
+    return arrStr.join("");
+}
+
+//kiểm tra xem ký tự bất kỳ đó có nằm trong mảng
+function checkCharacterInArray(str1, str2) {
+    let count = 0;
+    for (let i = 0; i < str1.length; i++) {
+        if (str1[i] === str2) {
+            count += 1;
+        }
+    }
+    if (count !== 0) {
+       return count;
+    } else {
+        return -1;
+    }
 }
